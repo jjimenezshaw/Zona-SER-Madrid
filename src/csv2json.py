6b0e17fd410ae13a27797b25db71e771b0dd3fdb
Javier Jimenez Shaw <j1@jimenezshaw.com>
2023-10-28 17:28:25 +0200

    first try

--- src/csv2json.py
+++ src/csv2json.py
@@ -6,6 +6,16 @@ import csv
 import geojson
 import json
 
+import os
+import errno
+from osgeo import ogr, osr
+
+def silentremove(filename):
+    try:
+        os.remove(filename)
+    except OSError as e: # this would be "except OSError, e:" before Python 2.6
+        if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
+            raise # re-raise exception if a different error occurred
 
 def getPoint(row):
     x = float(row['gis_x'].replace(',', '.'))
@@ -32,6 +42,87 @@ def addFeature(feature, zona, featureColls):
         coll = []
         featureColls[zona] = coll
     coll.append(feature)
+
+def tempLayer(name, srs=None, options=[]):
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    memDS = driver.CreateDataSource('/vsimem/'+name+'.shp')
+    layer = memDS.CreateLayer(name, srs=srs, options=options)
+    return (layer, memDS)
+
+
+def run2(zipfile, parquimetros, outputgeojson, out_zonas_filename):
+    shp_driver = ogr.GetDriverByName('ESRI Shapefile')
+    inDataSource = shp_driver.Open('/vsizip/' + zipfile, 0)
+    layers = inDataSource.GetLayerCount()
+    for lid in range(0,layers):
+        inLayer = inDataSource.GetLayer(lid)
+        print(inLayer.GetName())
+    inLayer = inDataSource.GetLayer('Limite_Barrios_Zona_SER')
+    print(inLayer.GetFeatureCount())
+
+    bandasLayer = inDataSource.GetLayer('BANDAS_DE_APARCAMIENTO')
+    print(bandasLayer.GetFeatureCount())
+
+    silentremove(out_zonas_filename)
+    geojson_driver = ogr.GetDriverByName("GeoJSON")
+    outDataSource = geojson_driver.CreateDataSource(out_zonas_filename)
+
+    crs84=osr.SpatialReference()
+    crs84.SetFromUserInput('OGC:CRS84')
+    utm30=osr.SpatialReference()
+    utm30.SetFromUserInput('EPSG:32630')
+    outLayer = outDataSource.CreateLayer("", srs=crs84, options=['COORDINATE_PRECISION=7'])
+
+    (tmpoutLayer, tmpoutDS) = tempLayer('tmpout', crs84, options=['SHPT=POLYGON'])
+    fld_CODDIS = ogr.FieldDefn('CODDIS', ogr.OFTString)
+    tmpoutLayer.CreateField(fld_CODDIS)
+    fld_NOMDIS = ogr.FieldDefn('NOMDIS', ogr.OFTString)
+    tmpoutLayer.CreateField(fld_NOMDIS)
+    fld_CODBAR = ogr.FieldDefn('CODBAR', ogr.OFTString)
+    tmpoutLayer.CreateField(fld_CODBAR)
+    fld_NOMBAR = ogr.FieldDefn('NOMBAR', ogr.OFTString)
+    tmpoutLayer.CreateField(fld_NOMBAR)
+    fld_name = ogr.FieldDefn('name', ogr.OFTString)
+    tmpoutLayer.CreateField(fld_name)
+
+    for inl in inLayer:
+        if inl.GetFieldAsString('CODBAR') == '-':
+            continue
+
+        o = '../web/bandas_' + inl.GetFieldAsString('CODDIS') + inl.GetFieldAsString('CODBAR')+'.geojson'
+        silentremove(o)
+        outDataSourceClipped = geojson_driver.CreateDataSource(o)
+        outLayerClipped = outDataSourceClipped.CreateLayer('', srs=crs84, options=['COORDINATE_PRECISION=7'])
+
+        (tmpLayer, tmpDS) = tempLayer('tmp', utm30, options=['SHPT=ARC'])
+
+        (memLayer, memDS) = tempLayer('clipper', utm30)
+
+        memLayer.CreateFeature(inl)
+        ogr.Layer.Clip(bandasLayer, memLayer, tmpLayer)
+        print(tmpLayer.GetFeatureCount())
+        fld_name = ogr.FieldDefn('zona', ogr.OFTString)
+        tmpLayer.CreateField(fld_name)
+        zona = '' + inl.GetFieldAsString('CODDIS') + inl.GetFieldAsString('CODBAR')
+
+        for fid in range(tmpLayer.GetFeatureCount()):
+            f = tmpLayer.GetFeature(fid)
+            geo = f.GetGeometryRef()
+            geo.TransformTo(crs84)
+            f.SetField('zona', zona)
+            outLayerClipped.CreateFeature(f)
+
+        geo = inl.GetGeometryRef()
+        geo.TransformTo(crs84)
+        tmpoutLayer.CreateFeature(inl)
+
+    for inl in tmpoutLayer:
+        print(inl.GetFieldCount())
+        name = inl.GetFieldAsString('CODDIS') + inl.GetFieldAsString('CODBAR') + ' - ' + inl.GetFieldAsString('NOMBAR')
+        inl.SetField('name', name)
+        outLayer.CreateFeature(inl)
+
+
 
 def run(inputcsv, parquimetros, outputgeojson, encoding):
     featureColls = {}
@@ -100,9 +191,9 @@ def run(inputcsv, parquimetros, outputgeojson, encoding):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parsea los ficheros csv de plazas y parquímetros de la Zona SER Madrid.')
-    parser.add_argument('-i', '--input', dest='plazas',
-                        default='../sources/Listado_plazas_SER_2023_08.csv',
-                        help='fichero csv de plazas de la Zona SER Madrid "madrid.es: Zonas SER. Distintivos y parquímetros"')
+    parser.add_argument('-i', '--input', dest='zip',
+                        default='../sources/BARRIOS_APARCAMIENTOS_SER.zip',
+                        help='TODO')
     parser.add_argument('-p', '--parquimetros', dest='parquimetros',
                         default='../sources/Parquimetros_2023_08.csv',
                         help='fichero csv de parquímetros de la Zona SER Madrid')
@@ -118,4 +209,6 @@ if __name__ == '__main__':
     # https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=65d85d6f40b86710VgnVCM2000001f4a900aRCRD
 
     args = parser.parse_args()
-    run(args.plazas, args.parquimetros, args.outputgeojson, args.encoding)
+    # run(args.plazas, args.parquimetros, args.outputgeojson, args.encoding)
+    out_zonas='../web/zonas_ser.geojson'
+    run2(args.zip, args.parquimetros, args.outputgeojson, out_zonas)
